ab05b6151dbae83a40510dce43046da4eee73db8
Adit Murali <adit.murali@outlook.com>
2020-06-10 11:35:29 -0700

    fixed bug in modified memory

--- memory.py
+++ memory.py
@@ -122,7 +122,7 @@ class ReplayMemory():
     # Calculate truncated n-step discounted return R^n = Σ_k=0->n-1 (γ^k)R_t+k+1 (note that invalid nth next states have reward 0)
     #TODO add cases here - this is why reward schedule doesn't work
     if self.progress_reward:
-      R = torch.tensor([transition[self.history-1]], dtype=torch.float32, device=self.device)
+      R = torch.tensor([transition[self.history-1].reward], dtype=torch.float32, device=self.device)
     elif self.spot_trial_reward:
       raise NotImplementedError()
     else:
